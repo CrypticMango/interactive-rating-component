bdd53c837d67295266077a0aa0905efb6c5e5a21
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-07-27 19:34:44 -0600

    added commented TODO list

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 //THIS WORKS!! YAS!!! :D
 
+//TO DO
+//-insert rating number into thank you pages when submit is pressed
+//-when submit is pressed show thank you page and hide rating page
+//-bring user back to main page after thank you page has been shown
+
 let submitButton = document.getElementById('submit-button');
 let ratingButton = document.getElementById('rating-button')
 
@@ -13,3 +18,5 @@ function pressedRating() {
 
 submitButton.onclick = pressedSubmit();
 submitButton.onclick = pressedRating();
+
+pressedSubmit(none);
