f4554d967cdd2081a5b263623238c1df9b8bb91a
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-07-27 19:26:13 -0600

    started working on the JS rating functions

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-let submitButton = getElementById('submit-button');
+//THIS WORKS!! YAS!!! :D
+
+let submitButton = document.getElementById('submit-button');
+let ratingButton = document.getElementById('rating-button')
+
+function pressedSubmit() {
+    alert('you have pressed submit');
+}
+
+function pressedRating() {
+    alert('you have pressed a rating button')
+}
+
+submitButton.onclick = pressedSubmit();
+submitButton.onclick = pressedRating();
