6c2b3f631aed98ad8cef71ab395edce5ee77c87d
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-07-29 18:04:15 -0600

    changed card elements from class to ID
    -built function to hide rating-start and show thank-you-page when submit is pressed
    -created variables

--- app.js
+++ app.js
@@ -2,20 +2,24 @@
 
 //TO DO
 //-insert rating number into thank you pages when submit is pressed
-//-when submit is pressed, show thank you page and hide rating page
+//-when submit is pressed, show thank you page and hide rating page ✔
 //-bring user back to main page after thank you page has been shown
 //-when rating button is clicked, should be light grey before submitting
 
 let submitButton = document.getElementById('submit-button');
-let ratingButton = document.getElementById('rating-button')
+let ratingButton = document.getElementById('rating-button');
+
 
 function pressedSubmit() {
-    alert('you have pressed submit');
+    //alert('you have pressed submit');
+    document.getElementById("selected-rating").innerHTML = "You selected 20 out of 5";
+    let thanksPage = document.getElementById('thank-you-page');
+    thanksPage.style.display = "block";
+    let ratingPage = document.getElementById('rating-start');
+    ratingPage.style.display = "none";
 }
 
 function pressedRating() {
-    alert('you have pressed a rating button')
+    alert('you have pressed a rating button');
 }
 
-submitButton.onclick = pressedSubmit();
-submitButton.onclick = pressedRating();
