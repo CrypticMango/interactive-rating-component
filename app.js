26e5dabccd16bafc4c4489875283f87218178843
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-08-01 15:16:50 -0600

    function for button background

--- app.js
+++ app.js
@@ -6,23 +6,54 @@
 //-bring user back to main page after thank you page has been shown
 //-when rating button is clicked, should be light grey before submitting
 
-let submitButton = document.getElementById('submit-button');
-let ratingButton = document.getElementById('rating-button');
+'use strict';
 
+let submitButton = document.getElementById('submit-button');
+let ratingButton = document.querySelectorAll(".rating-button");
+
+//handles submit button----------------------------------
 
 function pressedSubmit() {
     //alert('you have pressed submit');
-    document.getElementById("selected-rating").innerHTML = "You selected 20 out of 5";
+    //document.getElementById("selected-rating").innerHTML = "You selected 20 out of 5";
     let thanksPage = document.getElementById('thank-you-page');
     thanksPage.style.display = "block";
     let ratingPage = document.getElementById('rating-start');
     ratingPage.style.display = "none";
+    let selectedRating = document.getElementById('selected-rating');
+    selectedRating.innerHTML = "YAY";
 }
 
-function pressedRating() {
-    //when a rating number is pressed insert the number into the thank you page.
-    //alert('you have pressed a rating button');
-    let ratingNumber = getElementsByClassName("rating-button");
-    
+//handles rating buttons --------------------------------
+
+function pressedRatingOne() {
+    let buttonColorOne = document.getElementById('rating-button-one');
+    buttonColorOne.style.backgroundColor = '#D3D3D3';
 }
 
+function pressedRatingTwo() {
+    let buttonColorTwo = document.getElementById('rating-button-two');
+    buttonColorTwo.style.backgroundColor = '#D3D3D3';
+}
+
+function pressedRatingThree() {
+    let buttonColorThree = document.getElementById('rating-button-three');
+    buttonColorThree.style.backgroundColor = '#D3D3D3';
+}
+
+function pressedRatingFour() {
+    let buttonColorFour = document.getElementById('rating-button-four');
+    buttonColorFour.style.backgroundColor = '#D3D3D3';
+}
+
+function pressedRatingFive() {
+    let buttonColorFive = document.getElementById('rating-button-five');
+    buttonColorFive.style.backgroundColor = '#D3D3D3';
+}
+
+
+//ratingButton.addEventListener('click'), handleButtonColor();
+
+//function handleButtonColor() {
+   // ratingButton.style.color = 'rgb(255,192,203)';
+//};
