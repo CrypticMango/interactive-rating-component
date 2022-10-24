e904fdc21ff2772743c1e8fa5bd442277918ccae
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-10-24 07:22:57 -0600

    added focus state to buttons
    -removed code not needed
    -added function for reload of page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //TO DO
 //-insert rating number into thank you page when submit is pressed
 //-when submit is pressed, show thank you page and hide rating page ✔
-//-bring user back to main page after thank you page has been shown
+//-bring user back to main page after thank you page has been shown 
 //-when rating button is clicked, should be light grey before submitting
 
 let submitButton = document.getElementById("submit-button");
@@ -15,28 +15,16 @@ function pressedSubmit() {
 	thanksPage.style.display = "block";
 	let ratingPage = document.getElementById("rating-start");
 	ratingPage.style.display = "none";
+	return true;
 }
 
-//handles rating buttons --------------------------------
+const timeout = setTimeout(returnToMain, 2000);
 
-let button = document.getElementById("button1");
-button.addEventListener("click", chosenRating);
-
-function chosenRating() {
-	document.getElementById("button1").style.background = "#FFC0CB";
+function returnToMain() {
+	if (pressedSubmit === true) {
+		location.reload();
+	}
 }
 
-console.log(button);
-console.log(placeholderNumber);
-console.log(ratingNumber);
+returnToMain();
 
-//function pressedRating() {
-//let buttonColor = document.getElementById//('rating-button');
-//buttonColor.style.backgroundColor = '#D3D3D3';
-//}
-
-//ratingButton.addEventListener('click'), handleButtonColor();
-
-//function handleButtonColor() {
-// ratingButton.style.color = 'rgb(255,192,203)';
-//};
