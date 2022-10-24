0d7afed7bc8e37b81277db494ab56aa7bab749fb
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-10-24 06:53:35 -0600

    added button id

--- app.js
+++ app.js
@@ -4,33 +4,39 @@
 //-bring user back to main page after thank you page has been shown
 //-when rating button is clicked, should be light grey before submitting
 
-let submitButton = document.getElementById('submit-button');
+let submitButton = document.getElementById("submit-button");
 let placeholderNumber = 3;
-let ratingNumber = document.querySelector('.rating-number');
+let ratingNumber = document.querySelector(".rating-number");
 
 //handles submit button----------------------------------
 
 function pressedSubmit() {
-    let thanksPage = document.getElementById('thank-you-page');
-    thanksPage.style.display = "block";
-    let ratingPage = document.getElementById('rating-start');
-    ratingPage.style.display = "none";
+	let thanksPage = document.getElementById("thank-you-page");
+	thanksPage.style.display = "block";
+	let ratingPage = document.getElementById("rating-start");
+	ratingPage.style.display = "none";
 }
 
 //handles rating buttons --------------------------------
 
-let button = document.querySelectorAll(".rating-button");
+let button = document.getElementById("button1");
+button.addEventListener("click", chosenRating);
+
+function chosenRating() {
+	document.getElementById("button1").style.background = "#FFC0CB";
+}
+
 console.log(button);
 console.log(placeholderNumber);
 console.log(ratingNumber);
 
 //function pressedRating() {
-    //let buttonColor = document.getElementById//('rating-button');
+//let buttonColor = document.getElementById//('rating-button');
 //buttonColor.style.backgroundColor = '#D3D3D3';
 //}
 
 //ratingButton.addEventListener('click'), handleButtonColor();
 
 //function handleButtonColor() {
-   // ratingButton.style.color = 'rgb(255,192,203)';
+// ratingButton.style.color = 'rgb(255,192,203)';
 //};
